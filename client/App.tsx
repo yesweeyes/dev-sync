61e91c2198d0aff15a9d57588c6f7d32a23d3a30
Shyam <shyamsundarv00073@gmail.com>
2025-03-14 17:42:17 +0530

    add: gluestack ui lib

--- client/App.tsx
+++ client/App.tsx
@@ -1,23 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import './global.css';
-import Home from './src/screens/Home/page';
+import { StatusBar } from "expo-status-bar";
+import "@/global.css";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import { StyleSheet, Text, View } from "react-native";
+import "./global.css";
+import Home from "./src/screens/Home/page";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* <Text>Open up App.tsx to start working on your app!</Text> */}
-      {/* <StatusBar style="auto" />*/}
-      <Home/>
-    </View>
+    <GluestackUIProvider mode="light">
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <Home />
+      </View>
+    </GluestackUIProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- client/babel.config.js
+++ client/babel.config.js
@@ -1,9 +1,24 @@
 module.exports = function (api) {
   api.cache(true);
+  api.cache(true);
   return {
     presets: [
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
+    ],
+
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+
+          alias: {
+            "@": "./",
+            "tailwind.config": "./tailwind.config.js",
+          },
+        },
+      ],
     ],
   };
 };
