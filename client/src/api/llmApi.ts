df5de759fbc04de6512e59fb722f464c438e031f
Shyam <shyamsundarv00073@gmail.com>
2025-03-21 20:17:27 +0530

    delete: client

--- client/src/api/llmApi.ts
+++
@@ -1,8 +0,0 @@
-import api from "./config"
-import { HEALTHCHECK_LLM } from "./routes/llm"
-
-const llmApi = {
-    healthcheck: () => api.get(HEALTHCHECK_LLM),
-}
-
-export default llmApi;
--- client/src/api/routes/llm.ts
+++
@@ -1,1 +0,0 @@
-export const HEALTHCHECK_LLM = `/healthcheck-llm`;
