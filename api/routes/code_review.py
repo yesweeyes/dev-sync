84997dfe576acb2929807cc61c7f9f08e21f179d
Shyam <shyamsundarv00073@gmail.com>
2025-04-20 19:41:04 +0530

    refactor: file

--- api/routes/code_review.py
+++ api/routes/code_review.py
@@ -9,6 +9,15 @@ from services.code_review import (
     update_code_review_file as update_code_review_file_service,
     delete_code_review_file as delete_code_review_file_service
 )
+from services.code_review import (
+    get_code_review_file_by_id as get_code_review_file_by_id_service
+)
+from services.project import (
+    get_project as get_project_service
+)
+from utils.code_review import (
+    push_to_jira as code_review_push_to_jira_util
+)
 
 router = APIRouter(
     prefix="/code_review",
@@ -46,3 +55,11 @@ def delete_code_review_file(code_review_file_id: uuid.UUID, db: Session = Depend
         return {"detail": "Code Review File deleted successfully"}
     except Exception as e:
         raise HTTPException(status_code=404, detail=str(e))
+    
+@router.post("/{code_review_id}/push")
+def push_coder_review_to_jira(code_review_id: uuid.UUID, db: Session=Depends(get_db)):
+    code_review = get_code_review_file_by_id_service(db, code_review_id)
+    project_id = code_review.project_id
+    project = get_project_service(db, project_id)
+    
+    return code_review_push_to_jira_util(code_review, project)
--- api/routes/reviews.py
+++ api/routes/reviews.py
@@ -4,15 +4,7 @@ from sqlalchemy.orm import Session
 from fastapi import APIRouter, HTTPException, Depends
 from fastapi.responses import FileResponse
 from database import get_db
-from services.code_review import (
-    get_code_review_file_by_id as get_code_review_file_by_id_service
-)
-from services.project import (
-    get_project as get_project_service
-)
-from utils.code_review import (
-    push_to_jira as code_review_push_to_jira_util
-)
+
 
 router = APIRouter(
     prefix="/reviews",
@@ -42,12 +34,3 @@ def serve_file(filename: str):
     media_type = media_types.get(ext, 'application/octet-stream')
 
     return FileResponse(file_path, media_type=media_type)
-
-
-@router.post("/{code_review_id}/push")
-def push_coder_review_to_jira(code_review_id: uuid.UUID, db: Session=Depends(get_db)):
-    code_review = get_code_review_file_by_id_service(db, code_review_id)
-    project_id = code_review.project_id
-    project = get_project_service(db, project_id)
-    
-    return code_review_push_to_jira_util(code_review, project)
