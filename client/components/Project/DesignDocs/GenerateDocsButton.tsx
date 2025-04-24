764abfc4ff03a697df9326379634a008bda1794f
Shyam <shyamsundarv00073@gmail.com>
2025-04-25 01:07:14 +0530

    fix: ui for design docs generate button

--- client/components/Project/DesignDocs/GenerateDocsButton.tsx
+++ client/components/Project/DesignDocs/GenerateDocsButton.tsx
@@ -2,15 +2,14 @@ import React from "react";
 import { Box } from "@/components/ui/box";
 import { Fab, FabIcon, FabLabel } from "@/components/ui/fab";
 import { Sparkles } from "lucide-react-native";
-import { useAppStore } from "@/store/store"
-
+import { useAppStore } from "@/store/store";
 
 const GenerateDesignDocs = () => {
-  const { project_id, generateTechDocs} = useAppStore();
+  const { project_id, generateTechDocs } = useAppStore();
 
   const handleGenerate = async () => {
     if (project_id) {
-      generateTechDocs({project_id});
+      generateTechDocs({ project_id });
     }
   };
 
@@ -18,9 +17,10 @@ const GenerateDesignDocs = () => {
     <Box>
       <Fab
         size="md"
-        placement="bottom right"
+        placement="bottom left"
+        className="bg-gradient-to-r from-blue-500 to-purple-600 text-white hover:scale-105 transition-transform"
         onPress={handleGenerate}
-       >
+      >
         <FabIcon as={Sparkles} />
         <FabLabel>Generate Design Documents</FabLabel>
       </Fab>
